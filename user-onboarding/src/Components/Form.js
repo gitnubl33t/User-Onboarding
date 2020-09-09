76d57d525aa4104c1a245f2373b30c1443cd8e9d
gitnubl33t <jessicad8695@gmail.com>
2020-09-08 21:33:37 -0700

    updated readme

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -39,7 +39,7 @@ export default function Form() {
 
       <label htmlFor="password">
         Password:
-        <input type="text" name="password" />
+        <input type="password" name="password" />
       </label>
 
       <label htmlFor="tos">
